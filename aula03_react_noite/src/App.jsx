9dacbf48eef5aea02cd8adda5ca5b29b170be770
silvajpedro <silvajpro.31@gmail.com>
2023-09-15 20:55:18 -0300

    first commit

---
+++ aula03_react_noite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./header.jsx";
+// Aqui no componente principal App estamos importando
+// O componente Header que criamos
+// Esse é o conceito de componentização!!!
+// Arquivos separados utilizados de forma conjunta
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ aula03_react_noite/src/header.jsx
@@ -0,0 +1,14 @@
+//Componentes sempre devem ser a primeira letra maiuscula
+// Header
+// Esse é o componente Header para ele aparecer na tela devemos importar, ou seja trazer o componente Header para o componente principal App
+function Header(){
+    return(
+        <>
+            <h1>Sexta</h1>
+            <h2>Sábado</h2>
+            <h3>Domingo</h3>
+        </>
+    )
+}
+
+export default Header
